bea47e984847081bb8637e75eb4670f2d688a419
Jang Whemoon <palindrom615@gmail.com>
2020-04-06 02:02:04 +0900

    Add 17370 template

---
+++ src/boj17370.rs
@@ -0,0 +1,23 @@
+mod utils;
+use utils::*;
+
+fn solve(n: i32) -> i32 {
+    0
+}
+
+fn main() {
+    let n: i32 = read_line()[0];
+    println!("{}", solve(n));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        assert_eq!(solve(2), 0);
+        assert_eq!(solve(5), 2);
+        assert_eq!(solve(8), 8);
+    }
+}
