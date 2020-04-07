63f2a8b14e9370c4fbcc066d4b356dcb72f8a9e0
Jang Whemoon <palindrom615@gmail.com>
2020-04-07 16:32:22 +0900

    Fix boj2014

--- src/boj2014.rs
+++ src/boj2014.rs
@@ -4,60 +4,45 @@ use data_structures::smmh::*;
 use std::collections::HashSet;
 use utils::*;
 
-#[derive(Eq, PartialEq, Ord, PartialOrd, Debug)]
-struct Composite {
-    value: i32,
-    powers: Vec<u32>,
-}
-
-fn solve<'a>(k: usize, n: usize, primes: Vec<i32>) -> i32 {
+fn solve<'a>(n: usize, primes: Vec<i32>) -> i32 {
     let mut heap = SMMH::with_capacity(n);
-    let mut set = HashSet::new();
+    let mut set = HashSet::with_capacity(n);
 
-    heap.push(Composite {
-        value: 1,
-        powers: vec![0; primes.len()],
-    });
+    heap.push(1);
     for i in 0..n {
         if let Some(min) = heap.pop_min() {
-            set.remove(&format!("{:?}", &min.powers));
-            for idx in 0..k {
-                let mut power = min.powers.clone();
-                power[idx] += 1;
-                if set.contains(&format!("{:?}", power)) {
+            set.remove(&min);
+            let &max_val = heap.peek_max().unwrap_or(&i32::max_value());
+
+            println!("{}, {} {}", min, heap.len(), max_val);
+            for p in primes.iter() {
+                let new_val = min * p;
+                if set.contains(&new_val) {
                     continue;
                 }
-                let new_val = min.value * primes[idx];
-                if heap.len() > n - i + 1 {
-                    let max_val = heap.peek_max().unwrap();
-                    if new_val > max_val.value {
-                        continue;
+                let &max_val = heap.peek_max().unwrap_or(&i32::max_value());
+                if heap.len() > n  {
+                    if new_val > max_val {
+                        break;
+                    } else {
+                        set.remove(&heap.pop_max().unwrap());
                     }
                 }
-                set.insert(format!("{:?}", &power));
-                heap.push(Composite {
-                    value: new_val,
-                    powers: power,
-                });
-
-                if heap.len() > n - i + 1{
-                    let rm = heap.pop_max().unwrap();
-                    set.remove(&format!("{:?}", rm.powers));
-                }
+                set.insert(new_val);
+                heap.push(new_val);
             }
         }
     }
-    let min = heap.pop_min().unwrap();
-    min.value
+    heap.pop_min().unwrap()
 }
 
 fn main() {
     let line: Vec<usize> = read_line();
-    let k = line[0];
+    // let k = line[0];
     let n = line[1];
     let primes: Vec<i32> = read_line();
 
-    println!("{}", solve(k, n, primes));
+    println!("{}", solve(n, primes));
 }
 
 #[cfg(test)]
@@ -66,14 +51,13 @@ mod tests {
 
     #[test]
     fn examples() {
-        assert_eq!(solve(4, 19, vec![2, 3, 5, 7]), 27);
+        assert_eq!(solve(19, vec![2, 3, 5, 7]), 27);
     }
     #[test]
     fn memory() {
         assert_eq!(
             solve(
-                78,
-                100000,
+                397,
                 vec![
                     2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73,
                     79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157,
@@ -82,7 +66,7 @@ mod tests {
                     349, 353, 359, 367, 373, 379, 383, 389, 397
                 ]
             ),
-            343170
+            397
         );
     }
 }
