46be0bf021189622b2f03efdd90041463d025869
Jang Whemoon <palindrom615@gmail.com>
2020-04-30 01:58:52 +0900

    Refactor read_line fn

--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,11 @@
-use std::fmt::Debug;
 use std::str::FromStr;
 
-pub fn read_line<T: FromStr>() -> Vec<T>
-where
-    T::Err: Debug,
-{
+pub fn read_line<T: FromStr>() -> Vec<T> {
     let stdin = std::io::stdin();
     let mut line = String::new();
     let _ = stdin.read_line(&mut line);
     line.split_whitespace()
-        .map(|x| String::from(x).parse::<T>().unwrap())
+        .flat_map(|x| x.parse::<T>())
         .collect()
 }
 
